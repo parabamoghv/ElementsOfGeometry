8b8ff057cf766980717bcb91d79259a9020aa6b3
parabamoghv <parabamoghv@gmail.com>
2026-02-15 19:04:39 -0500

    removed junk

--- ElementsOfGeometry/todo.lean
+++
@@ -1,8 +0,0 @@
-theorem Elements.line_eq_of_pts (l : e.Lines) (A : e.Points) : A ∈ l ↔ ∃ B : e.Points, e.mk_line_pt_pt A B = l := by
-  constructor
-  · intro hA
-
-    sorry
-
-  · intro h
-    sorry
